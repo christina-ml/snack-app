ece9913f350251686d166f30b11b12f8ab12169a
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-04 12:56:52 -0400

    add snackCount, and button with onClick event to increase count

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      snackCount: 0,
+    }
+  }
+
+  giveSnack = () => {
+    this.setState({
+      snackCount: this.state.snackCount + 1
+    })
+  }
+
   render() {
-    return <div className="App">Snack App goes here!</div>;
+    return (
+    <div className="App">Snack App goes here!
+      <button onClick={ this.giveSnack }>Give snack</button>
+      <div>Baby Yoda has eaten { this.state.snackCount } snacks.</div>
+    </div>
+    );
   }
 }
 
