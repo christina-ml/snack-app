e72e1aa5681a0e117fa1f56e10b01f3b5df2e637
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-04 12:59:54 -0400

    add baby yoda image

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ class App extends React.Component {
 
   render() {
     return (
-    <div className="App">Snack App goes here!
+    <div className="App">
+      <h1>Give Baby Yoda some snack.</h1>
+      <img src="https://img.huffingtonpost.com/asset/5fb802c62900005b61c6c565.png?ops=1778_1000" 
+      alt="baby yoda" />
       <button onClick={ this.giveSnack }>Give snack</button>
       <div>Baby Yoda has eaten { this.state.snackCount } snacks.</div>
     </div>
